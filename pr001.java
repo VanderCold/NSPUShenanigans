9d931626e533fdf6e4929827efbeb846bf3909eb
VanderCold <vander.cold@mail.ru>
2025-10-15 11:41:35 +0700

    Демонстрация работы обобщений

---
+++ pr001.java
@@ -0,0 +1,25 @@
+class Gen<T> {
+	T ob;
+	Gen(T o) {
+		ob = o;
+	}
+
+	T getOb() {
+		return ob;
+	}
+
+	void shotType() {
+		System.out.println("Тип T: " + ob.getClass().getName());
+	}
+}
+class pr001 {
+	public static void main (String[] args) {
+		Get<Integer> iOb;
+		iOb = new Gen<Integer>(88);
+		iOb.showType();
+		int v = iOb.getOb();
+		System.out.println("Значение: " + v);
+
+		System.out.println();
+	}
+}
